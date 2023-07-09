b8c8abbe9e5f5c06ccc5eddf5626b2faab54220b
Yesid Gomez <wujibifan.hh@gmail.com>
2023-07-09 14:46:26 -0500

    Validación manual del formulario SignIn

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,23 +1,58 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import SignContext from "../context/SignContext";
 
 const SignIn = () => {
 
     const { setStep } = useContext(SignContext);
 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const [emailFocused, setEmailFocused] = useState(false);
+    const [passwordFocused, setPasswordFocused] = useState(false);
+
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+        console.log("Enviando información al servidor...");
+        
+        if (email && password) {
+            console.log(`${email} - ${password}`);
+        }
+    };
+
     return (
         <main>
         <h3>Hello, friend!</h3>
         <div className="card">
-            <form autoComplete="off">
+            <form onSubmit={handlerSubmit} autoComplete="off">
             <fieldset>
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" autoFocus />
+                <input 
+                type="email"
+                id="email"
+                autoFocus
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                onBlur={() => setEmailFocused(true)}
+                // Atributo personalizado
+                data-focused={emailFocused}
+                />
                 <p className="error">Valid email required</p>
             </fieldset>
             <fieldset>
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
+                <input 
+                type="password"
+                id="password"     
+                required
+                // Mínimo 8 caracteres, una letra mayúscula, una minúscula y un número
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onBlur={() => setPasswordFocused(true)}
+                data-focused={passwordFocused}
+                />
                 <p className="error">Password required</p>
             </fieldset>
             <button type="submit">Login</button>
